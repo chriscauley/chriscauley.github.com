aa3fbf8baa3fed169069bc51a1cbd3c5671a9b2a
chriscauley <chris@lablackey.com>
2016-02-21 21:25:03 -0500

    improving JinjaReader and adding a blog post

--- pelicanconf.py
+++ pelicanconf.py
@@ -39,16 +39,29 @@ THEME = '/home/chriscauley/pelican-blog/landscape/'
 from pelican.readers import HTMLReader
 from jinja2 import FileSystemLoader
 from jinja2.environment import Environment
+import arrow
 
 class JinjaReader(HTMLReader):
+  def get_meta_data(self,text):
+    print 1
+    lines = [l.strip() for l in text.split("<body>")[0].split('\n') if l.strip()]
+    print lines
+    out = {}
+    for line in lines:
+      i = line.index(":")
+      out[line[:i]] = line[i+1:]
+    for k,v in out:
+      if k in ['date','modified']:
+        out[k] = arrow.get(value).datetime
+    return out
   def read(self,filename):
     env = Environment()
     BASE_DIR = "content/"
     TEMPLATE_DIR = BASE_DIR+"/".join(filename.split(BASE_DIR)[-1].split("/")[:-1])
     env.loader = FileSystemLoader(TEMPLATE_DIR)
-    template = env.get_template(filename.split(TEMPLATE_DIR)[-1])
-    body, metadata = super(JinjaReader,self).read(filename)
-    return template.render(),metadata
+    text = env.get_template(filename.split(TEMPLATE_DIR)[-1]).render()
+    metadata = self.get_meta_data(text)
+    return text, metadata
 
 READERS = {
   'html': JinjaReader
