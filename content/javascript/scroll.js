beea18668537502477d229f0c39a8e545803f933
chriscauley <chris@lablackey.com>
2016-12-09 14:09:50 -0500

    simplifying scroll header a bit

--- content/javascript/scroll.js
+++ content/javascript/scroll.js
@@ -1,21 +1,19 @@
-var header = document.querySelector("header");
-var last_scroll = 0;
-var header_y = -header.scrollHeight;
-window.addEventListener("scroll",function() {
-  var fixed = false;
-  if (window.scrollY<2*header.scrollHeight) {
-    //havent scrolled past header
-    header_y = -header.scrollHeight;
-  } else {
+(function() {
+  var header = document.querySelector("header");
+  var last_scroll = 0;
+  var header_y = 0;
+  var header_h = header.scrollHeight; // needs to be in the listener if your header changes size
+  window.addEventListener("scroll",function() {
+    if (window.scrollY == 0) {
+      header.className = ""; // you may prefer header.classList.remove("fixed")
+      last_scroll = header.style.top = 0;
+      return
+    }
     header.className = "fixed"; // you may prefer header.classList.add("fixed")
     header_y = header_y - (window.scrollY-last_scroll);
     header_y = Math.min(header_y,0);
-    header_y = Math.max(header_y,-header.scrollHeight);
+    header_y = Math.max(header_y,-header_h);
     header.style.top = header_y + "px";
-  }
-  last_scroll = window.scrollY;
-  if (window.scrollY == 0) {
-    header.className = ""; // you may prefer header.classList.remove("fixed")
-    header.style.top = 0;
-  }
-});
+    last_scroll = window.scrollY;
+  });
+})();
