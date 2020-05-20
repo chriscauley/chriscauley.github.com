2e77ac1121e405e979c50c1526578b978ed6644c
chriscauley <chris@lablackey.com>
2020-05-20 13:14:16 +0000

    removing old blog

--- _publish.sh
+++
@@ -1,2 +0,0 @@
-ghp-import -b master output
-git push origin master
--- start.sh
+++
@@ -1,5 +0,0 @@
-#currently not functioning... use two screens
-pelican -r &
-cd output &&
-python -m pelican.server 8889 &
-echo 1
