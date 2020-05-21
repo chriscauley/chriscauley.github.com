bcca9644ba70fc4d06668ff144e4e7af4cf03147
chriscauley <chris@lablackey.com>
2020-05-21 14:31:01 +0000

    adding hero image for serpinski

--- client/posts/css/serpinski/index.js
+++ client/posts/css/serpinski/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import Markdown from 'react-markdown'
-import { register } from '../../register'
-import Snippet from '../../components/Snippet'
+import { register } from '../../../register'
+import Snippet from '../../../components/Snippet'
+import Serpinski from './Serpinski'
 import { Tabs } from '@unrest/core'
+import './styles.css'
+
+const url = s => `/src/posts/css/serpinski/${s}`
 
 const content = `
 A pure css Serpinski Triangle I made. The each unit cell has three divs, and to extend the fractal one level you just add three more divs in each empty div. Each div is \`width: 50%; height: 50%;\` of the parent div. The divs are then floated, making a square with three squares inside of it, and the forth quadrant is empty.
@@ -10,23 +14,11 @@ A pure css Serpinski Triangle I made. The each unit cell has three divs, and to 
 Normally pure css trangles are made using elements with \`width: 0; height: 0\` and a border with asymetrick coloring. If you are unfamiliar with this technique, you can read more at [CSS Tricks](http://css-tricks.com/snippets/css/css-triangle/ "Pure CSS Triangle"). However, because borders can't be percentages, this doesn't work with fractals. Instead we use a square, rotated pseudo elements (the \`div:before\` in the above css). The parent of the pseudo element is overflow hidden. Here's the full CSS. Feel free to email me if you want to know more. I may post more on this later.
 `
 
-const Recurse3 = ({ depth, id }) => (
-  <div id={id}>
-    {depth > 0 && (
-      <>
-        <Recurse3 depth={depth - 1} />
-        <Recurse3 depth={depth - 1} />
-        <Recurse3 depth={depth - 1} />
-      </>
-    )}
-  </div>
-)
-
 function Component() {
   const tabs = {
-    demo: <Recurse3 id="serpinski-triangle" depth={4} />,
-    css: <Snippet src="/src/styles/_serpinski.css" />,
-    html: <Snippet src="/src/posts/css/_serpinski.html" />,
+    demo: <Serpinski id="serpinski-triangle" depth={5} />,
+    css: <Snippet src={url('styles.css')} />,
+    html: <Snippet src={url('pseudo.html')} />,
   }
 
   return (
@@ -39,7 +31,8 @@ function Component() {
 
 register({
   Component,
-  thumbnail: '/src/images/serpinski.png',
+  thumbnail: url('thumbnail.png'),
+  image: url('hero.png'),
   description: 'A recursive triforce using nothing but HTML and CSS.',
   title: 'CSS Serpinski Triangle',
   slug: 'serpinski',
